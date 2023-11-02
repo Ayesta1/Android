4a6d66c8644b0dc6532e9b7ec42619d941c05d45
ayesta <ayesta1@gmail.com>
2023-11-02 16:54:51 +0100

    data class creado

--- app/src/main/java/com/woodland/android/MainActivity.kt
+++ app/src/main/java/com/woodland/android/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.woodland.android
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import java.time.Year
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(usuario+", "+direccion)
     }
+    data class Usuario (val imagen:String = "", val nombre:String="", val apellido:String="",val email: String="", val direccion: String="", val FechaNacimiento:String ="")
+    data class Direccion (val calle:String="",val cp:String = "", val ciudad:String="")
+
+    var usuario:Usuario=Usuario("img","Jordi", "A", "a@a.a", "Repartidor", "10/05/1999")
+    var direccion:Direccion=Direccion("Repartidor", "08023","Barcelona" )
+
+
+
+
+
 }
